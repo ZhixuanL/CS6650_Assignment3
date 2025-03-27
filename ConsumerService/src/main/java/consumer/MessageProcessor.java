c6d6d975e9b2981c7ef337d718bacc94497fb63c
zhixuan <liu.zhixu@northeastern.edu>
2025-03-26 23:15:38 -0700

    server tested locally,successfully query data

--- ConsumerService/src/main/java/consumer/MessageProcessor.java
+++ ConsumerService/src/main/java/consumer/MessageProcessor.java
@@ -14,17 +14,26 @@ public class MessageProcessor {
       int seasonID = json.getInt("seasonID");
       int time = json.getInt("time");
 
+      JSONObject rideJson = new JSONObject();
+      rideJson.put("resortID", resortID);
+      rideJson.put("seasonID", seasonID);
+      rideJson.put("dayID", dayID);
+      rideJson.put("liftID", liftID);
+      rideJson.put("time", time);
+
+      String redisKey = "skier:" + skierID + ":day:" + dayID + ":time:" + time;
+
       try (Jedis jedis = new Jedis("localhost", 6379)) {
-        String redisKey = "skier:" + skierID + ":day:" + dayID + ":time:" + time;
-
-        JSONObject rideJson = new JSONObject();
-        rideJson.put("resortID", resortID);
-        rideJson.put("seasonID", seasonID);
-        rideJson.put("dayID", dayID);
-        rideJson.put("liftID", liftID);
-        rideJson.put("time", time);
-
         jedis.set(redisKey, rideJson.toString());
+
+        jedis.sadd("skierDays:" + skierID, String.valueOf(dayID));
+
+        jedis.hincrBy("skierVertical:" + skierID, String.valueOf(dayID), liftID * 10);
+
+        jedis.sadd("skierLifts:" + skierID + ":day:" + dayID, String.valueOf(liftID));
+
+        jedis.sadd("resortVisitors:" + resortID + ":day:" + dayID, String.valueOf(skierID));
+
         System.out.println("✔ Stored in Redis → " + redisKey);
       }
 
